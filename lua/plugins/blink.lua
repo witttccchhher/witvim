fc23f5726366ea65f9cfaa18a49771a592514b32
somokill <somokill@noreply.codeberg.org>
2024-12-28 20:46:43 +0500

    Some changes

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,6 +2,16 @@ return {
   "saghen/blink.cmp",
   lazy = false,
   version = "v0.*",
+  dependencies = {
+    "L3MON4D3/LuaSnip",
+    version = "v2.*",
+    dependencies = "rafamadriz/friendly-snippets",
+    opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+    config = function(_, opts)
+      require("luasnip").config.set_config(opts)
+      require "config.luasnip"
+    end
+  },
   opts = function()
     return require("config.blink")
   end
